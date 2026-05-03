3de495abafc45356d79e4a762b44068e1bcff22f
AdibAhmed317 <adib.ah.official069@gmail.com>
2026-05-03 15:59:27 +0600

    switched port

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite';
 import { devtools } from '@tanstack/devtools-vite';
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import netlify from '@netlify/vite-plugin-tanstack-start'; // ← add this
-
 import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tanstackStart(), viteReact(), netlify()],
+  plugins: [devtools(), tanstackStart(), nitro(), viteReact()],
 });
 
 export default config;
